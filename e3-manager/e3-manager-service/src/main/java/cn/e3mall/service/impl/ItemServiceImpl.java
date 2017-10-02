299e70e077f831a10d10c6d5bbb5e0bc54eb9dc8
xiaogejiangyoyoyo <1293759349@qq.com>
2017-10-02 20:20:56 +0800

    Initial commit

---
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -0,0 +1,32 @@
+package cn.e3mall.service.impl;
+
+import cn.e3mall.mapper.TbItemMapper;
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemExample;
+import cn.e3mall.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+    @Autowired
+    private TbItemMapper itemMapper;
+
+    @Override
+    public TbItem getItemById(long itemId) {
+        //根据主键查询
+        //TbItem tbItem = itemMapper.selectByPrimaryKey(itemId);
+        TbItemExample example = new TbItemExample();
+        TbItemExample.Criteria criteria = example.createCriteria();
+        //设置查询条件
+        criteria.andIdEqualTo(itemId);
+        //执行查询
+        List<TbItem> list = itemMapper.selectByExample(example);
+        if (list != null && list.size() > 0) {
+            return list.get(0);
+        }
+        return null;
+    }
+}
